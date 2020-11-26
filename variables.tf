7c45e670af5f8158deab417ec611a421f75c0b7a
AlexStrokov <62788259+AlexStrokov@users.noreply.github.com>
2020-11-26 16:57:24 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,1 @@
-variable "credentials" {
-  description = "Variable for github secret TF_VAR_CREDENTIALS"
-}
 
-variable "user" {
-  default = "root"
-}
-
-variable "project" {
-  default = "cosmic-slate-292412"
-}
-
-variable "region" {
-  default = "us-central1"
-}
